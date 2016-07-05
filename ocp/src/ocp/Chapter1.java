455b7e5a86d0ab2941b059329689542eea5d3c6a
woollsn <woollsn@PADL4Q8KFV1.misys.global.ad>
2016-07-05 10:16:49 +0100

    Adding ocp project

---
+++ ocp/src/ocp/Chapter1.java
@@ -0,0 +1,10 @@
+package ocp;
+
+public class Chapter1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ocp/src/ocp/Chapter3.java
@@ -0,0 +1,10 @@
+package ocp;
+
+public class Chapter3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
